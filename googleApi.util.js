259ffa501120e4beb8a991f4ff991a0ef713ec21
Narottam Jha <narottam2911@gmail.com>
2024-05-11 23:13:47 +0530

    minimal API for fiddle session impl done

--- googleApi.util.js
+++ googleApi.util.js
@@ -62,7 +62,10 @@ async function getUserAuthUrl(SCOPES = [], config) {
         ...config,
     })
 }
-
+/**
+ * Createa an auth client with client_id, client_secret, redirect_uri
+ * @returns {googleAuthLib.OAuth2Client}
+ */
 async function getAuthClient() {
     const {
         client_id: clientId,
@@ -86,6 +89,29 @@ async function folderExistsInDrive(accessToken) {
     const { client_secret: privateKey } = await getAppCredentials()
     const decryptedAccessToken = decryptToken(accessToken, privateKey)
     const queryString = `q=mimeType='application/vnd.google-apps.folder' and name='esfiddle' and trashed=false`
+    const response = await fetch(
+        `https://www.googleapis.com/drive/v3/files?${queryString}`,
+        {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${decryptedAccessToken}`,
+            },
+        }
+    )
+
+    const jsonResponse = await response.json()
+    log(`jsonResponse`, jsonResponse)
+    if (jsonResponse?.files?.length) {
+        // consumer should save this id
+        return jsonResponse?.files[0]?.id
+    } else return null
+}
+
+async function getFolderIdByName(accessToken, name, esfiddleFolderId) {
+    const log = logger(`getFolderIdByName`)
+    const { client_secret: privateKey } = await getAppCredentials()
+    const decryptedAccessToken = decryptToken(accessToken, privateKey)
+    const queryString = `q=mimeType='application/vnd.google-apps.folder' and name='${name}' and trashed=false and '${esfiddleFolderId}' in parents`
     const response = await fetch(
         `https://www.googleapis.com/drive/v3/files?${queryString}`,
         {
@@ -146,6 +172,111 @@ async function createAppFolderInDrive(accessToken) {
     }
 }
 
+async function updateFiddleSessionFolder(
+    accessToken,
+    fiddleName,
+    esfiddleFolderId
+) {
+    const log = logger(`createAppFolderInDrive`)
+    try {
+        const { client_secret: privateKey } = await getAppCredentials()
+        const decryptedAccessToken = decryptToken(accessToken, privateKey)
+
+        const existingFolderId = await getFolderIdByName(
+            accessToken,
+            fiddleName,
+            esfiddleFolderId
+        )
+        log(`existingFolderId`, existingFolderId)
+        let newFileId
+        if (!existingFolderId) {
+            const folderMetadata = {
+                name: fiddleName,
+                mimeType: 'application/vnd.google-apps.folder',
+                parents: [esfiddleFolderId],
+            }
+            /// create his folder in drive
+            const response = await fetch(
+                `https://www.googleapis.com/drive/v3/files`,
+                {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${decryptedAccessToken}`,
+                    },
+                    body: JSON.stringify(folderMetadata),
+                }
+            )
+            const jsonResponse = await response.json()
+            log(`jsonResponse`, jsonResponse)
+            const { id } = jsonResponse
+            newFileId = id
+        }
+        if (existingFolderId) return existingFolderId
+        else return newFileId
+    } catch (error) {
+        throw error
+    }
+}
+
+async function createInitialFiles(accessToken, fiddleSessionId) {
+    const log = logger(`createInitialFiles`)
+    const { client_secret: privateKey } = await getAppCredentials()
+    const decryptedAccessToken = decryptToken(accessToken, privateKey)
+    const files = {
+        'index.js': 'text/javascript',
+        'index.html': 'text/html',
+        'index.css': 'text/css',
+    }
+
+    for (const [name, mimeType] of Object.entries(files)) {
+        const fileMetadata = {
+            name,
+            mimeType,
+            // fiddle session folder id
+            parents: [fiddleSessionId],
+        }
+        let response
+
+        try {
+            response = await fetch(
+                'https://www.googleapis.com/drive/v3/files',
+                {
+                    method: 'POST',
+                    headers: {
+                        Authorization: `Bearer ${decryptedAccessToken}`,
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(fileMetadata),
+                }
+            ).then((res) => res.json())
+
+            if (!response.ok) {
+                throw new Error(
+                    `HTTP error! status: ${response.status}, ${
+                        response?.message || response?.error?.message
+                    }`
+                )
+            }
+
+            return response
+        } catch (error) {
+            log(`Error creating file`, error)
+            throw error
+        }
+    }
+}
+
+// TODO: update this to do the work
+// saving file delete newly created onece
+
+/**
+ * Converts filedata to readable stream and send to google drive api to save/update
+ * @param {Object} param0  - {filename: <string>, data: <string>}
+ * @param {String} accessToken  - encrypted access token
+ * @param {String} folderId - id of the parent folder
+ * @returns
+ */
 async function saveFileToGoogleDrive(
     { filename = '', data: fileData },
     accessToken,
@@ -174,9 +305,11 @@ async function saveFileToGoogleDrive(
         const drive = google.drive({ version: 'v3', auth })
         // send this to either update or create file utility
         const media = {
-            mimeType: 'text/javascript',
-            body: Readable.from(fileData),
+            mimeType: getMimeType(filename),
+            body: Readable.from(fileData.toString()),
         }
+
+        log(`media obj`, media)
 
         // checks the files in the folder
 
@@ -187,7 +320,7 @@ async function saveFileToGoogleDrive(
 
         log(`filesListResponse.data `, filesListResponse.data)
 
-        const filesArray = filesListResponse.data.files;
+        const filesArray = filesListResponse.data.files
         // files is just a representional entity
         // not to be confused with JS file entity
         const currentFileObj = filesArray.find(({ name }) => name === filename)
@@ -201,6 +334,7 @@ async function saveFileToGoogleDrive(
             const response = await drive.files.update({
                 fileId,
                 media,
+
                 fields: 'id, name, mimeType',
             })
             log('files.update.response.data', response.data)
@@ -210,7 +344,7 @@ async function saveFileToGoogleDrive(
             const response = await drive.files.create({
                 requestBody: {
                     name: filename,
-                    mimeType: 'text/javascript',
+                    mimeType: getMimeType(filename),
                     parents: [folderId],
                 },
                 media,
@@ -223,7 +357,7 @@ async function saveFileToGoogleDrive(
         }
     } catch (error) {
         console.error(error)
-        // throw error
+        throw error
     }
 }
 
@@ -237,28 +371,30 @@ async function syncFileDataFromDrive(folderId, accessToken) {
     try {
         const log = logger('syncFileDataFromDrive - params')
         log(`params`, `|${folderId}|`, `|${accessToken}|`)
-        const drive = await getDriveInstance(accessToken);
+        const drive = await getDriveInstance(accessToken)
         const response = await drive.files.list({
             q: `'${folderId}' in parents and trashed=false`,
-            fields: 'files(name, mimeType, id, modifiedTime)'
-        });
+            fields: 'files(name, mimeType, id, modifiedTime)',
+        })
 
-        log(`response`, response.data);
+        log(`response`, response.data)
 
-        const files = response.data.files;
+        const files = response.data.files
 
-        // retrive data for all files 
-        const filesWithData = await Promise.all(files.map(async (file) => {
-            const {id: fileId} = file;
-            const filesResponse = await drive.files.get({
-                fileId,
-                alt: 'media'
-            });
-            return {...file, data: filesResponse.data}
-        }));
-        return filesWithData;
+        // retrive data for all files
+        const filesWithData = await Promise.all(
+            files.map(async (file) => {
+                const { id: fileId } = file
+                const filesResponse = await drive.files.get({
+                    fileId,
+                    alt: 'media',
+                })
+                return { ...file, data: filesResponse.data }
+            })
+        )
+        return filesWithData
     } catch (error) {
-        throw error;
+        throw error
     }
 }
 
@@ -276,6 +412,64 @@ async function getDriveInstance(accessToken) {
     const drive = google.drive({ version: 'v3', auth })
     return drive
 }
+
+/**
+ * Takes an encrypted accessToken decrypts and validates it
+ * via oauth2
+ * @param {String} accessToken
+ * @returns
+ */
+async function validateAccessToken(accessToken) {
+    const log = logger(`validateAccessToken`)
+    try {
+        const { client_secret: privateKey } = await getAppCredentials()
+        const oauth2Client = await getAuthClient()
+        const decryptedAccessToken = decryptToken(accessToken, privateKey)
+        oauth2Client.setCredentials({ access_token: decryptedAccessToken })
+        const tokenInfo = await oauth2Client.getTokenInfo(decryptedAccessToken)
+
+        log(`Token valid for client`, tokenInfo.aud)
+        return true
+    } catch (error) {
+        log(`error validating access token`, error)
+        return false
+    }
+}
+
+/**
+ * since the filename are fixed
+ *
+ */
+ function getMimeType(filename) {
+    const log = logger(`getMimeType`)
+    log(`received filename `, filename)
+    const regex = /^[a-zA-Z][^/]*\.([^./]+)$/
+    const match = filename.match(regex)
+    const extension = match[1]
+
+    log(`extracted extension of file ${filename} -> ${extension}`)
+    let mimeType
+    switch (extension) {
+        case 'js':
+            mimeType = 'text/javascript'
+            break
+
+        case 'css':
+            mimeType = 'text/css'
+            break
+
+        case 'html':
+            mimeType = 'text/html'
+            break
+
+        default:
+            mimeType = 'text/plain'
+    }
+
+    log(` mimeType of file ${filename}`, mimeType)
+
+    return mimeType
+}
 // authorize().then(listFiles).catch(console.error);
 
 module.exports = {
@@ -283,7 +477,12 @@ module.exports = {
     getAppCredentials,
     getAuthClient,
     getUserAuthUrl,
+    getDriveInstance,
     createAppFolderInDrive,
+    updateFiddleSessionFolder,
+    createInitialFiles,
     saveFileToGoogleDrive,
     syncFileDataFromDrive,
+    validateAccessToken,
+    getMimeType,
 }
