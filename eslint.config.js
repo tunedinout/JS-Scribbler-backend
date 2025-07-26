2ffa6a4f2350b82e688885d4c756fd2619640449
Narottam Jha <narottam2911@gmail.com>
2025-07-26 19:40:42 +0530

    some more eslint can't hurt

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,39 @@
-// eslint.config.js
-const eslintPluginNode = require('eslint-plugin-node')
+// eslint.config.js  (flat config)
+const pluginNode = require('eslint-plugin-node')
+const pluginImport = require('eslint-plugin-import')
+const pluginUnused = require('eslint-plugin-unused-imports')
+const pluginSecurity = require('eslint-plugin-security')
+const pluginPromise = require('eslint-plugin-promise')
 
 module.exports = [
   {
     files: ['**/*.js'],
     ignores: ['node_modules/**', 'dist/**', '*.config.js'],
-    languageOptions: {
-      ecmaVersion: 2021,
-      sourceType: 'script'
-    },
+    languageOptions: { ecmaVersion: 2021, sourceType: 'script' },
     plugins: {
-      node: eslintPluginNode
+      node: pluginNode,
+      import: pluginImport,
+      'unused-imports': pluginUnused,
+      security: pluginSecurity,
+      promise: pluginPromise
     },
     rules: {
-      semi: ['off'],
+      semi: 'off',
       quotes: ['error', 'single'],
       indent: ['error', 2],
       'no-console': 'off',
-      'no-unused-vars': ['warn']
+      'no-unused-vars': 'warn',
+      'import/no-unresolved': 'error',
+      'unused-imports/no-unused-imports': 'warn',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        { vars: 'all', varsIgnorePattern: '^_', args: 'after-used', argsIgnorePattern: '^_' }
+      ],
+      'security/detect-object-injection': 'warn',
+      'security/detect-unsafe-regex': 'error',
+      'promise/always-return': 'warn',
+      'promise/no-return-wrap': 'warn',
+      'promise/no-nesting': 'warn'
     }
   }
 ]
